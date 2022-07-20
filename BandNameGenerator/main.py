45fb0d46df40e1440f6f971ba25b294334e2daa4
Lordicode <lordicode@gmail.com>
2022-07-21 00:39:46 +0200

    First commit

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,9 @@
+""""This is the Day 1 project
+Very Easy
+Band Name Generator"""
+
+print("Hello! Now that you are serious about your band, what will be the name?")
+print("Let's ease the job for you!")
+city_name = input("What city you grew up in? ->\n")
+pet_name = input("Name of your pet? ->\n")
+print("Your cool new band name is: \n" + city_name + " " + pet_name)
