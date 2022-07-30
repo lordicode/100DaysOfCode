791392fab135b59df20287b4f64ec1675ee3258f
Lordicode <lordicode@gmail.com>
2022-07-30 18:03:49 +0200

    DAY 4 Rock Paper Scissors. Finished with a lot of if/else.

--- DAY 4 - Rock Paper Scissors/main.py
+++ DAY 4 - Rock Paper Scissors/main.py
@@ -1,14 +1,20 @@
-#using System Random for a more random result, avoiding using simple Marsenne Twister for more or less
+# using System Random for a more random result, avoiding using simple Marsenne Twister for more or less
 import random
+#call module with art
 import ascii_art
 
 foo = random.SystemRandom()
 
 signs = ["Tree", "Human", "Snake",
-         "Scissors", "Fire", "Rock", "Gun", "Lightning", "Devil", "Dragon", "Water", "Air", "Paper", "Sponge", "Wolf", ]
+         "Scissors", "Fire", "Rock", "Gun", "Lightning", "Devil", "Dragon", "Water", "Air", "Paper", "Sponge", "Wolf"]
 
-sign_index_player = input("What is your play here?\nGun, Lightning, Devil, Dragon, Water, Air, Paper, Sponge, Wolf, Tree, Human, Snake, Scissors, Fire, Rock?\nEnter a number from 0 to 14->")
+sign_index_player = input(
+    "What is your play here?\nGun, Lightning, Devil, Dragon, Water, Air, Paper, Sponge, Wolf, Tree, Human, Snake, "
+    "Scissors, Fire, Rock?\nEnter a number from 0 to 14->")
 sign_index_player = int(sign_index_player)
+#quit with assert error if invalid input
+assert 14 >= sign_index_player >= 0
+#get ransom number from range
 sign_index_os = foo.randint(0, len(signs) - 1)
 os_sign = signs[sign_index_os]
 player_sign = signs[sign_index_player]
@@ -18,19 +24,35 @@ print(f"\nYou have chosen: {player_sign}, {sign_index_player}\n", ascii_art.asci
 
 if sign_index_os == sign_index_player:
     print("It is a draw!\n")
-if sign_index_os + sign_index_player > len(signs):
-    print("special rules")
-elif (sign_index_os > sign_index_player) or (sign_index_player == 14 and sign_index_os <= 6):
-    print("You lost!\n")
-elif (sign_index_os < sign_index_player) or (sign_index_player != 14 and sign_index_os > 6):
-    print("You won!\n")
-#else:
-#    print("You won!\n")
-"""
-if index is 0 and enemy is 14, then 0 win
-if index of enemy sign is 6 higher than of yours, yours win, else enemy win
-if get to the end of list go -1....
-so your is 3 - dragon, it beats 2, 1, 0, 14, 13, 12, 11. It is beaten by 4, 5, 6, 7, 8, 9, 10
-if you have chosen 14 - wolf, then it beats 13, 12, 11, 10, 9, 8, 7 and is beaten by 0, 1, 2, 3, 4, 5, 6
-if you have chosen 13 - sponge, then it beats 12, 11, 10, 9, 8, 7, 6 and is beaten by 14, 0, 1, 2, 3, 4, 5, 6"""
-
+elif sign_index_player == 0 and sign_index_os <= 7:
+    print("You won!")
+elif sign_index_player == 1 and (14 > sign_index_os >= 9 or sign_index_os == 0):
+    print("You won!")
+elif sign_index_player == 2 and (10 <= sign_index_os <= 14 or sign_index_os <= 1):
+    print("You won!")
+elif sign_index_player == 3 and (11 <= sign_index_os <= 14 or sign_index_os <= 2):
+    print("You won!")
+elif sign_index_player == 4 and (12 <= sign_index_os <= 14 or sign_index_os <= 3):
+    print("You won!")
+elif sign_index_player == 5 and ((sign_index_os == 14 or sign_index_os == 13) or (sign_index_os < sign_index_player)):
+    print("You won!")
+elif sign_index_player == 6 and (0 <= sign_index_os < sign_index_player or sign_index_os == 14):
+    print("You won!")
+elif sign_index_player == 7 and sign_index_os < sign_index_player:
+    print("You won!")
+elif sign_index_player == 8 and (1 <= sign_index_os <= 7):
+    print("You won!")
+elif sign_index_player == 9 and (2 <= sign_index_os <= 8):
+    print("You won!")
+elif sign_index_player == 10 and (3 <= sign_index_os <= 9):
+    print("You won!")
+elif sign_index_player == 11 and (4 <= sign_index_os <= 10):
+    print("You won!")
+elif sign_index_player == 12 and (5 <= sign_index_os <= 11):
+    print("You won!")
+elif sign_index_player == 13 and (6 <= sign_index_os <= 12):
+    print("You won!")
+elif sign_index_player == 14 and (7 <= sign_index_os <= 13 or sign_index_os == 0):
+    print("You won!")
+else:
+    print("PC won!")
