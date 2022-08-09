448d312c090d838dbbb0420a0f46c027e64362e5
Lordicode <lordicode@gmail.com>
2022-08-10 00:46:40 +0200

    DAY 8 - Caesar cipher completed.

--- DAY 8 - Caesar Cipher/main.py
+++ DAY 8 - Caesar Cipher/main.py
@@ -1,3 +1,5 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u',
@@ -9,19 +11,32 @@ def caesar(text_message, shift_by, direction):
     if direction == "encode":
         for letter in text_message:
             position = alphabet.index(letter)
-            new_position = position + shift_by
+            if position + shift_by > len(alphabet):
+                new_position = (position + shift_by) % len(alphabet)
+            else:
+                new_position = position + shift_by
             encrypted_letter = alphabet[new_position]
             string_for_cipher += encrypted_letter
     elif direction == "decode":
         for letter in text_message:
             position = alphabet.index(letter)
-            new_position = position - shift_by
+            if position - shift_by < 0:
+                new_position = (position - shift_by) % len(alphabet)
+            else:
+                new_position = position - shift_by
             encrypted_letter = alphabet[new_position]
             string_for_cipher += encrypted_letter
     print(f"Resulting text is {string_for_cipher}")
 
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-caesar(text_message=text, shift_by=shift, direction=direction)
+print(art.art)
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text_message=text, shift_by=shift, direction=direction)
+    x = input("Do you want to go again? Yes or No")
+    x = x.lower()
+    if x == 'yes':
+        continue
+    else:
+        quit("Thank you for using Caesar Cipher!")
